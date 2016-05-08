8c4be4965b46b1b6f2e719b1acb84b08f646019a
lbkchen <lbkchen@gmail.com>
2016-05-08 02:10:42 -0700

    refined praw scraping

--- python/parse_comments.py
+++ python/parse_comments.py
@@ -3,35 +3,41 @@ __author__ = 'kenchen'
 import praw
 import csv
 
-# FUNCTIONS
-
 
 def combine_subreddits(subs):
     return "+".join(subs)
 
 
-r = praw.Reddit("Comment parser v1.0 by u/Kurumia")
+r = praw.Reddit("Network visualization data scraper v1.0 by u/k-a-n")
 
 subreddits = ["funny", "AdviceAnimals", "pics", "aww", "WTF",
               "todayilearned", "gaming", "videos", "gifs", "leagueoflegends"]
 
+# Getting top [limit] posts from multi-reddit defined from subreddits above
 top_subreddits = r.get_subreddit(combine_subreddits(subreddits))
 top_posts = top_subreddits.get_top_from_all(limit=100)
 
+# Write posts csv table
 with open('posts.csv', 'wb') as csv_posts:
-    post_attrs = ["id", "subreddit_id", "subreddit", "title", "created_utc", "ups", "downs", "gilded"]
+    post_attrs = ["id", "subreddit_id", "subreddit", "author", "title", "created_utc", "ups", "downs", "gilded"]
     pwriter = csv.writer(csv_posts, delimiter=',')
     pwriter.writerow(post_attrs)
 
+    # Write comments csv table
     with open('comments.csv', 'wb') as csv_comments:
-        comment_attrs = ["id", "parent_id", "subreddit", "created_utc", "ups", "downs", "gilded"]
+        comment_attrs = ["id", "parent_id", "subreddit", "author", "created_utc", "ups", "downs", "gilded"]
         cwriter = csv.writer(csv_comments, delimiter=',')
         cwriter.writerow(comment_attrs)
 
         for post in top_posts:
+            # Write this post with variables post_attrs to file
             pwriter.writerow([getattr(post, attr) for attr in post_attrs])
+
+            # Replace MoreComments objects with more Comments (expand comment tree deeper)
             post.replace_more_comments(limit=post.num_comments // 200)
             post_comments = praw.helpers.flatten_tree(post.comments)
+
+            # Write all Comment objects in the flattened comment tree
             for comment in post_comments:
                 if type(comment) != praw.objects.MoreComments:
                     cwriter.writerow([getattr(comment, attr) for attr in comment_attrs])
